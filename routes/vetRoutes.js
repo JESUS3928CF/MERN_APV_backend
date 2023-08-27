9d2c5d76ed970f10d97461b686bbc8ebc524dc5c
Jesús CF <jesus3928cf@gmail.com>
2023-08-27 08:24:47 -0500

    se corrigo bug en el import estava mal escrito la ruta del controlador y además falto el.js

--- routes/vetRoutes.js
+++ routes/vetRoutes.js
@@ -1,23 +1,33 @@
-import express from "express";
-import {updatePassword ,updateProfile, register, profile, confirm, authenticate, lostPassword, checkToken, newPassword} from "../controllers/vetController";
-import checkAuth from "../middleware/authMiddleware.js";
+import express from 'express';
+import {
+    updatePassword,
+    updateProfile,
+    register,
+    profile,
+    confirm,
+    authenticate,
+    lostPassword,
+    checkToken,
+    newPassword,
+} from '../controllers/vetController.js';
+import checkAuth from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 /// Área publica
-router.post('/', register );
-router.get("/confirm/:token", confirm );
-router.post("/login", authenticate);
+router.post('/', register);
+router.get('/confirm/:token', confirm);
+router.post('/login', authenticate);
 
-router.post("/lost-password", lostPassword);
+router.post('/lost-password', lostPassword);
 
 //- Simplificar peticiones que comparten la misma ruta
 router.route('/lost-password/:token').get(checkToken).post(newPassword);
 
 /// Área privada
-router.get('/profile', checkAuth , profile);
+router.get('/profile', checkAuth, profile);
 router.put('/profile/:id', checkAuth, updateProfile);
 //! Creando la ruta para actualizar
-router.put("/update-password", checkAuth, updatePassword);
+router.put('/update-password', checkAuth, updatePassword);
 
 export default router;
